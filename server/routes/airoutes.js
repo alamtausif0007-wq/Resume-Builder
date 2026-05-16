c23a259745ded54a666561ee98f229f1d18447c6
alamtausif0007-wq <alamtausif0007@gmail.com>
2026-05-16 10:50:11 +0530

    Delete server directory

--- server/routes/airoutes.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const router=express.Router()
-const aiCtrl=require('../controllers/aiController')
-const protect=require('../middlewares/authMiddleware')
-const ApiLimiter=require('../middlewares/RateLimiter')
-router.post('/enhance-pro-sum',protect,ApiLimiter,aiCtrl.enhanceProfessionalSummary)
-router.post('/enhance-job-desc',protect,ApiLimiter,aiCtrl.enhanceJobDescription)
-router.post('/upload-resume',protect,aiCtrl.uploadResume)
- module.exports=router
--- server/routes/paymentroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const protect=require('../middlewares/authMiddleware')
-const paymentController = require("../controllers/PaymentController");
-
-router.post("/create-order",protect, paymentController.createOrder);
-router.post("/verify",protect, paymentController.verifyPayment);
-
-module.exports = router;
